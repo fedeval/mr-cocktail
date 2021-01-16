ef78b110dbe7854fea14bb530cf62982dd738bdd
Federico Valenza <federico.valenza5@gmail.com>
2021-01-16 14:56:56 +0100

    cocktail new and create

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -7,9 +7,27 @@ class CocktailsController < ApplicationController
 
   def show; end
 
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
   private
 
   def find_cocktail
     @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
   end
 end
