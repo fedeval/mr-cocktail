c20af8b76a69fd4cf3eb5fbe562482c5997dc9ef
Federico Valenza <federico.valenza5@gmail.com>
2021-01-19 20:31:33 +0100

    added review system

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,24 @@
+class ReviewsController < ApplicationController
+  before_action :find_cocktail, only: [:create]
+
+  def create
+    @review = Review.new(review_params)
+    @review.cocktail = @cocktail
+    if @review.save
+      @review.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
+end
