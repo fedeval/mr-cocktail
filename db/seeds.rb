549408bacbd139d573ffc0eb5abd1bcc08318b1f
fedeval <federico.valenza5@gmail.com>
2021-03-16 19:41:31 +0100

    refactor seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,49 @@
 require 'open-uri'
 require 'json'
 
-url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-response = open(url).read
-ingredients = JSON.parse(response)
-
 puts 'clean up db'
 Ingredient.destroy_all
-puts 'generate seeds...'
+Cocktail.destroy_all
+Dose.destroy_all
+User.destroy_all
+
+puts 'create test users'
+User.create(email: 'federico@test.com', password: '123456')
+User.create(email: 'christina@test.com', password: '123456')
+User.create(email: 'ilaria@test.com', password: '123456')
+
+puts 'generate ingredients'
+ingredients_url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_response = open(ingredients_url).read
+ingredients = JSON.parse(ingredients_response)
 ingredients['drinks'].each { |ingredient| Ingredient.create!(name: ingredient['strIngredient1']) }
+
+puts 'generate cocktails'
+cocktails_url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail'
+cocktails_response = open(cocktails_url).read
+cocktails = JSON.parse(cocktails_response)
+
+cocktails['drinks'].sample(20).each do |cocktail|
+  new_cocktail = Cocktail.new(name: cocktail['strDrink'])
+  thumbnail = URI.open(cocktail['strDrinkThumb'])
+  new_cocktail.photo.attach(io: thumbnail, filename: 'nes.png', content_type: 'image/png')
+  new_cocktail.save
+
+  cocktail_url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=#{cocktail['idDrink']}"
+  cocktail_response = open(cocktail_url).read
+  cocktail_info = JSON.parse(cocktail_response)
+  
+  ingredient_num = 1
+  cocktail_info['drinks'].first.each do |key, value|
+    if key.include?('strIngredient') && !value.nil?
+      Dose.create(
+        cocktail: new_cocktail,
+        ingredient: Ingredient.find_by(name: value),
+        description: cocktail_info['drinks'].first["strMeasure#{ingredient_num}"]
+      )
+      ingredient_num += 1
+    end
+  end
+end
+
 puts 'done'
