8a2be25c0a5e05fb4def7c4468efada502fd810d
fedeval <federico.valenza5@gmail.com>
2021-03-22 23:22:23 +0100

    feat: add recipe column to cocktail tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_23_212141) do
+ActiveRecord::Schema.define(version: 2021_03_22_222028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2021_01_23_212141) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "recipe"
   end
 
   create_table "doses", force: :cascade do |t|
