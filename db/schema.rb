a9f81999aeb5daade037b5044ac7315135a9fac2
fedeval <federico.valenza5@gmail.com>
2021-03-25 00:01:04 +0100

    feat: add reference to user_id in cocktails table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_22_222028) do
+ActiveRecord::Schema.define(version: 2021_03_24_225906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2021_03_22_222028) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "recipe"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_cocktails_on_user_id"
   end
 
   create_table "doses", force: :cascade do |t|
@@ -81,6 +83,7 @@ ActiveRecord::Schema.define(version: 2021_03_22_222028) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "cocktails", "users"
   add_foreign_key "doses", "cocktails"
   add_foreign_key "doses", "ingredients"
   add_foreign_key "reviews", "cocktails"
