145d2cd0c0b48970011b7c0fbd2d159ca9c3bd43
fedeval <federico.valenza5@gmail.com>
2021-03-25 23:56:23 +0100

    fix: add association between users and favorites

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :cocktails
+  has_many :favorites
   
   validates :first_name, presence: true
   validates :last_name, presence: true
