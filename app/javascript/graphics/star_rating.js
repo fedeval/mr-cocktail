9d612b62b7e1b99837c888236c9c68730e5b8e17
fedeval <federico.valenza5@gmail.com>
2021-03-31 00:40:07 +0200

    style: add star rating on dashboard favorites as well

--- app/javascript/graphics/star_rating.js
+++ app/javascript/graphics/star_rating.js
@@ -2,6 +2,7 @@ const starRating = () => {
   const starRatingDivs = document.querySelectorAll('.star-rating');
   const fullStar = "<i class=\"fas fa-star\"></i>";
   const emptyStar = "<i class=\"far fa-star\"></i>";
+  const halfStar = "<i class=\"fas fa-star-half-alt\"></i>"
   starRatingDivs.forEach(starRatingDiv => {
     const ratingValue = parseInt(starRatingDiv.dataset.rating, 10);
     starRatingDiv.innerHTML = `
