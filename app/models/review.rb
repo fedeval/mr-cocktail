4f8a6ebed04a587e4fc3da5ccc0f3d4d3bfbfec9
fedeval <federico.valenza5@gmail.com>
2021-03-30 23:38:23 +0200

    feat: add user to reviews and validate

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :cocktail
+  belongs_to :user
 
   validates :content, presence: true
   validates :rating, inclusion: { in: 1..5 }, presence: true
+  validates :user, presence: true, uniqueness: { scope: :cocktail }
 end
